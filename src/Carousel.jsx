09c8ae1151b7daec6d1be86dfedf32809b07f053
Muhammad Hany <dev.mhany@gmail.com>
2023-11-05 02:41:57 +0200

    thumbnail updated

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -7,9 +7,15 @@ class Carousel extends Component {
   static defaultProps = {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
+  handleIndexClick = (e) => {
+    this.setState({
+      active: +e.target.dataset.index,
+    });
+  };
   render() {
     const { active } = this.state;
     const { images } = this.props;
+
     return (
       <div className="carousel">
         <img src={images[active]} alt="animal hero" />
@@ -17,6 +23,8 @@ class Carousel extends Component {
           {images.map((photo, index) => (
             // eslint-disable-next-line
             <img
+              onClick={this.handleIndexClick}
+              data-index={index}
               key={photo}
               src={photo}
               className={index === active ? "active" : ""}
