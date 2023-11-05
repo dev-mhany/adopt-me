da0b7d6e5ed3871f44fa075ffd98090eb883f195
Muhammad Hany <dev.mhany@gmail.com>
2023-11-05 02:43:39 +0200

    change photo fixed

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -4,31 +4,33 @@ class Carousel extends Component {
   state = {
     active: 0,
   };
+
   static defaultProps = {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
-  handleIndexClick = (e) => {
+
+  handleIndexClick = (event) => {
     this.setState({
-      active: +e.target.dataset.index,
+      active: +event.target.dataset.index,
     });
   };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
-
     return (
       <div className="carousel">
-        <img src={images[active]} alt="animal hero" />
+        <img src={images[active]} alt="animal" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
             // eslint-disable-next-line
             <img
-              data-index={index}
               key={photo}
               src={photo}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
-              onClick={() => this.setState({ active: index })}
+              onClick={this.handleIndexClick}
+              data-index={index}
             />
           ))}
         </div>
@@ -36,4 +38,5 @@ class Carousel extends Component {
     );
   }
 }
+
 export default Carousel;
