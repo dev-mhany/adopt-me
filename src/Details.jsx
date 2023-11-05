82b8fc9817cc809969644327aab56ad69b7b3de6
Muhammad Hany <dev.mhany@gmail.com>
2023-11-05 03:15:35 +0200

    end of 28- Error Boundaries

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
 
@@ -29,5 +30,12 @@ const Details = () => {
     </div>
   );
 };
+function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
 
-export default Details;
+export default DetailsErrorBoundary;
